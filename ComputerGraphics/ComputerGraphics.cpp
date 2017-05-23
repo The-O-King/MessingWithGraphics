3a937e25bd646768ca5a0f99325796117be7bcf3
The-O-King <techmanozmar@gmail.com>
2017-05-23 17:11:07 -0400

    Added BMP output Functionality
    Thanks to EasyBMP, output image now saves as Test.bmp in the source
    folder so you can see the output!

--- ComputerGraphics/ComputerGraphics.cpp
+++ ComputerGraphics/ComputerGraphics.cpp
@@ -5,13 +5,14 @@
 #include <vector>
 #include <iostream>
 #include "Vec3.h"
+#include "EasyBMP/EasyBMP.h"
 
 using namespace std;
 
 struct Rectangle {
 	vector<Vec3<float>> points; //topLeft, topRight, bottomLeft, bottomRight;
 	Vec3<float> normal;
-	int color;
+	RGBApixel color;
 };
 
 struct Ray {
@@ -62,20 +63,23 @@ bool pointInPoly(Rectangle &obj, Vec3<float> &point) {
 }
 
 //Currently Calculates Intersect with infinite plane of polygon, need to check bounds of polygon first!
-bool intersect(Rectangle &obj, Ray primaryRay, Vec3<float> *pHit) {
+bool intersect(Rectangle &obj, Ray primaryRay, Vec3<float> &pHit) {
 	float denom = primaryRay.direction.dotProd(obj.normal);
 
 	if (abs(denom) > 0.0001) /*may need to fix for float point error*/ {
 		float num = (obj.points[0] - primaryRay.startPoint).dotProd(obj.normal);
 		float dist = num / denom;
-		pHit = &(primaryRay.startPoint + primaryRay.direction * dist); //May be deallocated and cause pointer issues
-		return pointInPoly(obj, *pHit);
+		pHit = primaryRay.startPoint + primaryRay.direction * dist; //May be deallocated and cause pointer issues
+		return pointInPoly(obj, pHit);
 	}
 	else 
 		return false;
 }
 
 void raytrace() {
+	BMP output;
+	output.SetSize(1920, 1080);
+	output.SetBitDepth(24);
 	//Camera is at origin point
 	Vec3<float> camera = Vec3<float>(0);
 
@@ -83,17 +87,21 @@ void raytrace() {
 	vector<Rectangle> objects(1);
 	for (int k = 0; k < objects.size(); k++){
 		Rectangle *r = &objects[k];
-		//r->points = vector<Vec3<float>>(4);
-		//r->points[0] = Vec3<float>(-10, 10, 1.5);
-		//r->points[1] = Vec3<float>(10, 10, 1.5);
-		//r->points[2] = Vec3<float>(10, -10, 1.5);
-		//r->points[3] = Vec3<float>(-10, -10, 1.5);
-		r->points = vector<Vec3<float>>(3);
-		r->points[0] = Vec3<float>(-10, -10, 1.5);
-		r->points[1] = Vec3<float>(10, -10, 1.5);
-		r->points[2] = Vec3<float>(0, 20, 1.5);
-		r->normal = ((r->points[0] - r->points[1]).crossProd(r->points[1] - r->points[2])).normalize();
-		r->color = 1;
+		r->points = vector<Vec3<float>>(4);
+		r->points[0] = Vec3<float>(-500, 500, 10);
+		r->points[1] = Vec3<float>(500, 500, 10);
+		r->points[2] = Vec3<float>(500, -500, 1.5);
+		r->points[3] = Vec3<float>(-500, -500, 1.5);
+
+		//r->points = vector<Vec3<float>>(3);
+		//r->points[0] = Vec3<float>(-50, -10, 1.5);
+		//r->points[1] = Vec3<float>(50, -10, 5);
+		//r->points[2] = Vec3<float>(0, 75, 1.5);
+
+		r->normal = ((r->points[1] - r->points[0]).crossProd(r->points[2] - r->points[1])).normalize();
+		r->color.Red = 255;
+		r->color.Green = 0;
+		r->color.Blue = 0;
 	}
 
 
@@ -101,13 +109,15 @@ void raytrace() {
 	Ray primaryRay;
 	Ray shadowRay;
 	PointLight sourceLight;
-	sourceLight.location = Vec3<float>(0, 30, 3);
+	RGBApixel currColor;
+	sourceLight.location = Vec3<float>(0, 0, 0);
 	sourceLight.intensity = 2;
-	sourceLight.maxDistance = 100;
-	//Viewing plane is 80x80 and 1 unit in front camera in the Z direction
-	for (int j = 40; j > -40; j--) {
-		for (int i = -40; i < 40; i++) {
-			currPixel = Vec3<float>(i, j, 2);
+	sourceLight.maxDistance = 500;
+	//Viewing plane is 1920x1080 and 2 unit in front camera in the Z direction
+	for (int j = 0; j < 1080 ; j++) {
+		for (int i = 0; i < 1920; i++) {
+
+			currPixel = Vec3<float>(i - 1920 / 2, 1080/2 - j, 2);
 			primaryRay.startPoint = camera;
 			primaryRay.direction = (currPixel - primaryRay.startPoint).normalize();
 			primaryRay.length = 100;
@@ -116,7 +126,7 @@ void raytrace() {
 			float minDist = INFINITY;
 			Rectangle *hit = NULL;
 			for (int k = 0; k < objects.size(); k++) {
-				if (intersect(objects[k], primaryRay, &pHit)) {
+				if (intersect(objects[k], primaryRay, pHit)) {
 					float dist = (camera - pHit).length();
 					if (dist < minDist) {
 						hit = &objects[k];
@@ -130,23 +140,36 @@ void raytrace() {
 				shadowRay.direction = (sourceLight.location - pHit).normalize();
 				bool shadowed = false;
 				for (int k = 0; k < objects.size(); k++) {
-					if (hit != &objects[k] && intersect(objects[k], shadowRay, &pHit)) {
+					if (hit != &objects[k] && intersect(objects[k], shadowRay, pHit)) {
 						shadowed = true;
 						break;
 					}
 				}
-
-				if (!shadowed)
-					cout << hit->color; //Later on when bmp works remove if and hit->color * (sourceLight.maxDistance - (sourceLight.location - pHit).length) / sourceLight.maxDistance
-				else
-					cout << "0";
+				
+				if (!shadowed) {
+					float intensity = (sourceLight.maxDistance - (sourceLight.location - pHit).length()) / sourceLight.maxDistance;
+					intensity = 0 > intensity ? 0 : intensity;
+					currColor.Red = hit->color.Red * intensity;
+					currColor.Green = hit->color.Green * intensity;
+					currColor.Blue = hit->color.Blue * intensity;
+					output.SetPixel(i, j, currColor);
+				}
+				else {
+					currColor.Red = 0;
+					currColor.Green = 0;
+					currColor.Blue = 0;
+					output.SetPixel(i, j, currColor);
+				}
 			}
-			else
-				cout << "0";
+			else {
+				currColor.Red = 0;
+				currColor.Green = 0;
+				currColor.Blue = 0;
+				output.SetPixel(i, j, currColor);
+			}
 		}
-		cout << endl;
 	}
-
+	output.WriteToFile("Test.bmp");
 }
 
 int main()
